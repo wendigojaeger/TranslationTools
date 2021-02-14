faef807584b4819cfacfc39622f1cd4bee4c6fab
Wendigo Jaeger <wendigojaegersama@gmail.com>
2021-02-14 11:35:15 -0500

    Sort & Remove using

--- LibWendigoJaegerTranslationTool/Data/AssemblyFileSettings.cs
+++ LibWendigoJaegerTranslationTool/Data/AssemblyFileSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WendigoJaeger.TranslationTool.Undo;
+﻿using WendigoJaeger.TranslationTool.Undo;
 
 namespace WendigoJaeger.TranslationTool.Data
 {
--- LibWendigoJaegerTranslationTool/Data/FontSettings.cs
+++ LibWendigoJaegerTranslationTool/Data/FontSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WendigoJaeger.TranslationTool.Data
+﻿namespace WendigoJaeger.TranslationTool.Data
 {
     public class FontSettings : RefObject
     {
