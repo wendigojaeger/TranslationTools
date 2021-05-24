75843201bca325dfac3f0522f734668572171f59
Wendigo Jaeger <wendigojaegersama@gmail.com>
2021-05-24 17:14:04 -0400

    Complete DataSettings Editor and fix DestinationEND RAM Address label in ScriptSettingsEditorScript

--- WendigoJaegerTranslationTool/Editors/DataSettingsEditor.xaml.cs
+++ WendigoJaegerTranslationTool/Editors/DataSettingsEditor.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using WendigoJaeger.TranslationTool.Controls;
 using WendigoJaeger.TranslationTool.Data;
+using WendigoJaeger.TranslationTool.Extractors;
+using Xceed.Wpf.Toolkit;
 
 namespace WendigoJaeger.TranslationTool.Editors
 {
@@ -12,6 +18,8 @@ namespace WendigoJaeger.TranslationTool.Editors
     [EditorFor(typeof(DataSettings))]
     public partial class DataSettingsEditor : BaseDataSettingsEditor
     {
+        static Type[] _cachedDataExtractorTypes = null;
+
         public override string WindowTitle => Instance.Name;
 
         public DataSettingsEditor()
@@ -38,6 +46,68 @@ namespace WendigoJaeger.TranslationTool.Editors
                 Mode = BindingMode.TwoWay
             };
             textPreviewPicker.SetBinding(RefObjectPtrControl.RefObjectPtrProperty, textPreviewBinding);
+
+            Binding sourceRAMBinding = new()
+            {
+                Source = Instance,
+                Path = new PropertyPath(nameof(Instance.SourceRAMAddress)),
+                Mode = BindingMode.TwoWay,
+            };
+            upDownSourceRAM.SetBinding(LongUpDown.ValueProperty, sourceRAMBinding);
+
+            Binding destinationRAMBinding = new()
+            {
+                Source = Instance,
+                Path = new PropertyPath(nameof(Instance.DestinationRAMAddress)),
+                Mode = BindingMode.TwoWay,
+            };
+            upDownDestinationRAM.SetBinding(LongUpDown.ValueProperty, destinationRAMBinding);
+
+            Binding destinationRAMEndBinding = new()
+            {
+                Source = Instance,
+                Path = new PropertyPath(nameof(Instance.DestinationEndRAMAddress)),
+                Mode = BindingMode.TwoWay,
+            };
+            upDownDestinationRAMEnd.SetBinding(LongUpDown.ValueProperty, destinationRAMEndBinding);
+
+            Binding entriesBinding = new()
+            {
+                Source = Instance,
+                Path = new PropertyPath(nameof(Instance.Entries)),
+                Mode = BindingMode.TwoWay,
+            };
+            upDownEntries.SetBinding(UIntegerUpDown.ValueProperty, entriesBinding);
+
+            if (_cachedDataExtractorTypes == null)
+            {
+                var query = from a in AppDomain.CurrentDomain.GetAssemblies()
+                            from t in a.GetTypes()
+                            where t.GetInterfaces().Contains(typeof(IDataExtractor))
+                            select t;
+
+                _cachedDataExtractorTypes = query.ToArray();
+            }
+
+            List<IDataExtractor> dataExtractors = new();
+
+            foreach (var type in _cachedDataExtractorTypes)
+            {
+                dataExtractors.Add((IDataExtractor)Activator.CreateInstance(type));
+            }
+            dataExtractors.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            comboDataExtractors.ItemsSource = dataExtractors;
+
+            Binding textExtractorBinding = new()
+            {
+                Source = Instance,
+                Path = new PropertyPath(nameof(Instance.DataExtractor)),
+                Mode = BindingMode.TwoWay,
+            };
+
+            comboDataExtractors.SetBinding(ComboBox.SelectedItemProperty, textExtractorBinding);
+            comboDataExtractors.SelectedIndex = dataExtractors.FindIndex(x => x.GetType().IsAssignableFrom(Instance.DataExtractor.GetType()));
 
             Instance.PropertyChanged -= updateWindowTitle;
             Instance.PropertyChanged += updateWindowTitle;
